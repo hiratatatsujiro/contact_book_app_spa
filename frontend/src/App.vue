028636975aa533d795fb2cb158d1c19905313577
平田達二郎 <hiratatoorujirou@hiratatoorujirounoMacBook-Pro.local>
2022-08-19 03:06:31 +0900

    router

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,19 @@
 <template>
   <v-app>
-    <div>
-      <v-app-bar dark app color="blue">
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>Contact Book App</v-app-bar-title>
-        <v-tabs class="d-flex justify-end align-right">
-          <v-tab> 新規登録 </v-tab>
-          <v-tab> ログイン </v-tab>
-        </v-tabs>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" fixed temporary>
-        <v-list nav dense>
-          <v-list-item-group>
-            <v-list-item v-for="menu in menus" :key="menu">
-              <v-list-item-title>{{ menu }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-    </div>
-    <v-main>
-      <v-img
-        v-bind:src="require('@/assets/junior-highschool.png')"
-        width="100%"
-        height="100%"
-      ></v-img>
-    </v-main>
-    <div>
-      <v-footer color="yellow" dark app class="d-flex justify-center align-center">
-        constact_book_app
-      </v-footer>
-    </div>
+    <HeaderPage />
+    <router-view />
+    <FooterPage />
   </v-app>
 </template>
 
 <script>
+import HeaderPage from "./components/HeaderPage.vue";
+import FooterPage from "./components/FooterPage.vue";
 export default {
   name: "App",
 
-  components: {},
-
-  data: () => ({
-    menus: ["連絡", "日記", "学級通信", "チャットルーム", "時間わり", "フリーマーケット"],
-    drawer: false,
-  }),
+  components: { HeaderPage, FooterPage },
 };
 </script>
 
-<style>
-v-main {
-  background-color: aquamarine;
-}
-</style>
+<style></style>
---
+++ frontend/src/components/HeaderPage.vue
@@ -0,0 +1,37 @@
+/* eslint-disable */
+<template>
+  <div>
+    <v-app-bar dark app color="blue">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Contact Book App</v-app-bar-title>
+      <v-tabs class="d-flex justify-end align-right">
+        <v-tab> 新規登録 </v-tab>
+        <v-tab> ログイン </v-tab>
+      </v-tabs>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item v-for="menu in menus" :key="menu">
+            <v-list-item-title>{{ menu }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+
+  components: {},
+
+  data: () => ({
+    menus: ["連絡", "日記", "学級通信", "チャットルーム", "時間わり", "フリーマーケット"],
+    drawer: false,
+  }),
+};
+</script>
+
+<style></style>
